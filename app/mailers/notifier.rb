06c8d61c14a46fb625841a462afa45a700469a45
Chris Curtis <cmc@tmbx.com>
2012-05-29 22:06:28 -0400

    Modifications for Margaret's requests

--- app/mailers/notifier.rb
+++ app/mailers/notifier.rb
@@ -3,7 +3,7 @@ class Notifier < ActionMailer::Base
   def raw_ballot_notification(raw_ballot)
     @raw_ballot = raw_ballot
     mail(  :to => ENV['BALLOT_EMAIL_DEST'] || "cmc@tmbx.com", 
-           :from => ENV['BALLOT_EMAIL_FROM'] || "eballot@washingtontheater.org", 
+           :from => raw_ballot.adjudicator_email || "eballot@washingtontheater.org", 
            :subject => "Ballot for #{raw_ballot.show_title} ")
   end
   
