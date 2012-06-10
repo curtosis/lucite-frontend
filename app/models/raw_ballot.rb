4e2087134ea7a7d0eb15e51828c2f1a4af4a1a3a
Chris Curtis <cmc@tmbx.com>
2012-06-10 09:09:07 -0400

    Added production_type as required field. Fixes #6.

--- app/models/raw_ballot.rb
+++ app/models/raw_ballot.rb
@@ -6,7 +6,7 @@ class RawBallot < ActiveRecord::Base
   accepts_nested_attributes_for :ballot_overall_scoreblock, :ballot_technical_scoreblock, :ballot_performer_scores
 
   validates_presence_of :show_title, :producing_company, :adjudicator_name, 
-	:adjudicator_email, :adjudicator_company, :date_viewed, :adjudicator_number
+	:adjudicator_email, :adjudicator_company, :date_viewed, :adjudicator_number, :production_type
 
 
  end
